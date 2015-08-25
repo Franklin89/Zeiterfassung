5620395c936d021b9bb463e376a4894ebdbf96bc
kreuzerk <kevin.kreuzer@hotmail.com>
2015-08-25 19:26:00 +0200

    kk: Erster Entwurf der Views hinzugefügt

---
+++ html5client/src/app/app.js
@@ -0,0 +1,41 @@
+/**
+ * Created by U110333 on 25.08.2015.
+ */
+'use strict';
+
+var zeiterfassungsapp = angular.module('zeiterfassung.ui', ['ui.router'])
+
+    .controller('MainController', ['$log', function($log) {
+
+        this.vm = this;
+        this.test = 'Greeting from The Incredibels';
+        this.goa = null;
+    }]);
+
+
+zeiterfassungsapp.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/timeRecording");
+
+    $stateProvider
+        .state('timeRecording', {
+            url: "/timeRecording",
+            templateUrl: "recording/timeRecording.html"
+            //controller: "MainController as vm"
+        })
+        .state('projectManagement', {
+            url: "/projectManagement",
+            templateUrl: "project/projectManagement.html"
+            //controller: "UserCtrl as vm"
+        })
+        .state('taskManagement', {
+            url: "/taskManagement",
+            templateUrl: "task/taskManagement.html"
+            //controller: "UserCtrl as vm"
+        })
+        .state('accountManagement', {
+            url: "/accountManagement",
+            templateUrl: "usertask/accountManagement.html"
+            //controller: "UserCtrl as vm"
+        });
+});
