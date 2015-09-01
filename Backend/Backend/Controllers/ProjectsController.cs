7a7e87bd3519e4bb6084f47ae962ecd0e0843580
Daniel Jovanovic <daniel.jovanovic@geowerkstatt.ch>
2015-09-01 11:36:42 +0200

    add UserTasks- and ProjectsController

---
+++ Backend/Backend/Controllers/ProjectsController.cs
@@ -0,0 +1,59 @@
+﻿using Backend.Database;
+using Backend.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Backend.Controllers
+{
+    public class ProjectsController : ApiController
+    {
+        // GET: api/Projects
+        public IEnumerable<Project> Get()
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Projects.ToList();
+            }
+        }
+
+        // GET: api/Projects/5
+        public Project Get(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Projects.SingleOrDefault(t => t.Id == id);
+            }
+        }
+
+        // POST: api/Projects
+        public void Post([FromBody]Project Project)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Projects.Add(Project);
+                db.SaveChanges();
+            }
+        }
+
+        // PUT: api/Projects/5
+        public void Put(int id, [FromBody]Project Project)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Entry(db.Projects.Find(id)).CurrentValues.SetValues(Project);
+                db.SaveChanges();
+            }
+        }
+
+        // DELETE: api/Projects/5
+        public void Delete(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Projects.Remove(db.Projects.Find(id));
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Backend/Backend/Controllers/UserTasksController.cs
@@ -0,0 +1,59 @@
+﻿using Backend.Database;
+using Backend.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Backend.Controllers
+{
+    public class UserTasksController : ApiController
+    {
+        // GET: api/UserTasks
+        public IEnumerable<UserTask> Get()
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.UserTasks.ToList();
+            }
+        }
+
+        // GET: api/UserTasks/5
+        public UserTask Get(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.UserTasks.SingleOrDefault(t => t.Id == id);
+            }
+        }
+
+        // POST: api/UserTasks
+        public void Post([FromBody]UserTask UserTask)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.UserTasks.Add(UserTask);
+                db.SaveChanges();
+            }
+        }
+
+        // PUT: api/UserTasks/5
+        public void Put(int id, [FromBody]UserTask UserTask)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Entry(db.UserTasks.Find(id)).CurrentValues.SetValues(UserTask);
+                db.SaveChanges();
+            }
+        }
+
+        // DELETE: api/UserTasks/5
+        public void Delete(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.UserTasks.Remove(db.UserTasks.Find(id));
+                db.SaveChanges();
+            }
+        }
+    }
+}
