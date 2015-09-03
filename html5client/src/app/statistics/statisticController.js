e86f7f018652eb68dc50881dcd25c2945c00e1a2
kreuzerk <kevin.kreuzer@hotmail.com>
2015-09-03 07:13:16 +0200

    kk: Statistic View Tabelle hinzugefügt und mit Angular verknüpft

---
+++ html5client/src/app/statistics/statisticController.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Kiwi on 02.09.15.
+ */
+
+(function(){
+
+    var timerecordingapp = angular.module('zeiterfassung.ui');
+
+    timerecordingapp.controller('StatisticsController', function(){
+
+        var sc = this;
+
+        sc.timerecords;
+
+        var records = [
+            {
+              date: "02.01.2015",
+              hours: 8,
+              task: "Programmieren",
+              project: "Testproject"
+            },
+            {
+              date: "02.01.2015",
+              hours: 8,
+              task: "Programmieren",
+              project: "Testproject"
+            },
+            {
+              date: "02.01.2015",
+              hours: 8,
+              task: "Programmieren",
+              project: "Testproject"
+            },
+            {
+              date: "02.01.2015",
+              hours: 8,
+              task: "Programmieren",
+              project: "Testproject"
+            },
+            {
+                date: "02.01.2015",
+                hours: 8,
+                task: "Programmieren",
+                project: "Testproject"
+            },
+            {
+                date: "02.01.2015",
+                hours: 8,
+                task: "Programmieren",
+                project: "Testproject"
+            },
+        ];
+
+        function limittimerecordsTo4(){
+            sc.timerecords = records.slice(0,4);
+        }
+
+        sc.showallrecords = function(){
+            sc.timerecords = records;
+        }
+
+        limittimerecordsTo4();
+    });
+})();
+
