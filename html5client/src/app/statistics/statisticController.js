635497c884c91542213c56b764011568d05f965c
kreuzerk <kevin.kreuzer@hotmail.com>
2015-09-06 17:14:28 +0200

    kk: Draw chart in eine Funktion ausgelagert

--- html5client/src/app/statistics/statisticController.js
+++ html5client/src/app/statistics/statisticController.js
@@ -59,103 +59,88 @@
             sc.timerecords = records;
         };
 
+
+        var drawChartwithD3 = function () {
+            var width = 360;
+            var height = 360;
+            var radius = Math.min(width, height) / 2;
+            var donutWidth = 75;
+            var legendRectSize = 18;
+            var legendSpacing = 4;
+
+            var color = d3.scale.category20b();
+
+            var svg = d3.select('#chart')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height)
+                .append('g')
+                .attr('transform', 'translate(' + (width / 2) +
+                ',' + (height / 2) + ')');
+
+            var arc = d3.svg.arc()
+                .innerRadius(radius - donutWidth)
+                .outerRadius(radius);
+
+            var pie = d3.layout.pie()
+                .value(function (d) {
+                    return d.hours;
+                })
+                .sort(null);
+
+            var path = svg.selectAll('path')
+                .data(pie(records))
+                .enter()
+                .append('path')
+                .attr('d', arc)
+                .attr('fill', function (d, i) {
+                    return color(d.data.task);
+                });
+
+
+            var tooltip = d3.select('#chartcontainer');
+
+            path.on('mouseover', function (d) {
+                var total = d3.sum(records.map(function (d) {
+                    return d.hours;
+                }));
+                var percent = Math.round(1000 * d.data.hours / total) / 10;
+
+                $('#labelLabel').text("Task: " + d.data.task);
+                $('#countLabel').text("Stunden: " + d.data.hours);
+                $('#percentLabel').text(percent + '%');
+
+            });
+
+            var legend = svg.selectAll('.legend')
+                .data(color.domain())
+                .enter()
+                .append('g')
+                .attr('class', 'legend')
+                .attr('transform', function (d, i) {
+                    var height = legendRectSize + legendSpacing;
+                    var offset = height * color.domain().length / 2;
+                    var horz = -2 * legendRectSize;
+                    var vert = i * height - offset;
+                    return 'translate(' + horz + ',' + vert + ')';
+                });
+
+            legend.append('rect')
+                .attr('width', legendRectSize)
+                .attr('height', legendRectSize)
+                .style('fill', color)
+                .style('stroke', color);
+
+            legend.append('text')
+                .attr('x', legendRectSize + legendSpacing)
+                .attr('y', legendRectSize - legendSpacing)
+                .text(function (d) {
+                    return d;
+                });
+        }
+
+        drawChartwithD3();
         limittimerecordsTo4();
-
-
-        //Hier kommt der Data Visualisation Stuff
-        //=============================================
-
-
-        var dataset = [
-            {label: 'Abulia', count: 10},
-            {label: 'Betelgeuse', count: 20},
-            {label: 'Cantaloupe', count: 30},
-            {label: 'Dijkstra', count: 40}
-        ];
-
-        var width = 360;
-        var height = 360;
-        var radius = Math.min(width, height) / 2;
-        var donutWidth = 75;
-        var legendRectSize = 18;
-        var legendSpacing = 4;
-
-        var color = d3.scale.category20b();
-
-        var svg = d3.select('#chart')
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .append('g')
-            .attr('transform', 'translate(' + (width / 2) +
-            ',' + (height / 2) + ')');
-
-        var arc = d3.svg.arc()
-            .innerRadius(radius - donutWidth)
-            .outerRadius(radius);
-
-        var pie = d3.layout.pie()
-            .value(function (d) {
-                return d.hours;
-            })
-            .sort(null);
-
-        var path = svg.selectAll('path')
-            .data(pie(records))
-            .enter()
-            .append('path')
-            .attr('d', arc)
-            .attr('fill', function (d, i) {
-                return color(d.data.task);
-            });
-
-
-        var tooltip = d3.select('#chartcontainer');
-
-        path.on('mouseover', function (d) {
-            var total = d3.sum(records.map(function (d) {
-                return d.hours;
-            }));
-            var percent = Math.round(1000 * d.data.hours / total) / 10;
-
-            $('#labelLabel').text("Task: " + d.data.task);
-            $('#countLabel').text("Stunden: " + d.data.hours);
-            $('#percentLabel').text(percent + '%');
-
-        });
-
-        /*
-        path.on('mouseout', function () {
-            tooltip.style('display', 'none');
-        });
-        */
-
-
-        var legend = svg.selectAll('.legend')
-            .data(color.domain())
-            .enter()
-            .append('g')
-            .attr('class', 'legend')
-            .attr('transform', function (d, i) {
-                var height = legendRectSize + legendSpacing;
-                var offset = height * color.domain().length / 2;
-                var horz = -2 * legendRectSize;
-                var vert = i * height - offset;
-                return 'translate(' + horz + ',' + vert + ')';
-            });
-
-        legend.append('rect')
-            .attr('width', legendRectSize)
-            .attr('height', legendRectSize)
-            .style('fill', color)
-            .style('stroke', color);
-
-        legend.append('text')
-            .attr('x', legendRectSize + legendSpacing)
-            .attr('y', legendRectSize - legendSpacing)
-            .text(function (d) {
-                return d;
-            });
     });
 })();
 
