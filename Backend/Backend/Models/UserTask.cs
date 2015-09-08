77f549f90069c4c75a3c666cb2b32c2b7575779e
Matteo Locher <matteo.locher@ml-software.ch>
2015-09-08 11:03:26 +0200

    Update Model

--- Backend/Backend/Models/UserTask.cs
+++ Backend/Backend/Models/UserTask.cs
@@ -26,6 +26,9 @@ namespace Backend.Models
         public int ProjectId { get; set; }
         public virtual Project Project { get; set; }
 
+        public int TaskId { get; set; }
+        public virtual Task Task { get; set; }
+
         public double Time { get; set; }
 
         public DateTime Date{ get; set; }
