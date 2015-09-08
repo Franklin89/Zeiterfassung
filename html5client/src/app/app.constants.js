0ed8a26cd2f6e8be8d8f4dbdf7988f84c7ad9cf1
Daniel Jovanovic <daniel.jovanovic@geowerkstatt.ch>
2015-09-08 16:51:04 +0200

    consume authentication service and prepare login page

--- html5client/src/app/app.constants.js
+++ html5client/src/app/app.constants.js
@@ -11,5 +11,6 @@ angular.module('zeiterfassung.ui.app.constants', [])
         PROJECTS: 'https://interfaces.azurewebsites.net/api/projects',
         USERS: 'https://interfaces.azurewebsites.net/api/users',
         TASKS: 'https://interfaces.azurewebsites.net/api/tasks',
-        USERTASKS: 'https://interfaces.azurewebsites.net/api/usertasks'
+        USERTASKS: 'https://interfaces.azurewebsites.net/api/usertasks',
+        ACCOUNT: 'https://interfaces.azurewebsites.net/api/account'
     });
--- html5client/src/app/authentication/authentication.integration.service.js
+++ html5client/src/app/authentication/authentication.integration.service.js
@@ -1,21 +1,19 @@
-angular.module('zeiterfassung.authentication.integrationservices', [])
-
-    .factory('AuthenticationIntegrationService', function ($http, $log, $q, REST) {
-
-        function createSession(user) {
+angular.module('zeiterfassung.authentication.integrationservices', ['zeiterfassung.ui.app.constants'])
+    .factory('AuthenticationIntegrationService', ['$http', '$log', '$q', 'REST', function ($http, $log, $q, REST) {
+        function login(user) {
             var dfd = $q.defer();
-            $log.debug('create session for user: ' + angular.toJson(user, true));
-            $http.post(REST.SESSIONS, user, {tracker: 'rest'})
+            $log.debug('login user: ' + angular.toJson(user, true));
+            $http.post(REST.ACCOUNT + '/login', {Username: user.name, PasswordHash: user.password})
                 .success(function (result) {
                     dfd.resolve(result);
                 })
-                .error(function (result, status, headers, config) {
+                .error(function (result, status) {
                     dfd.reject({result: result, status: status});
                 });
             return dfd.promise;
         }
 
         return {
-            createSession: createSession
+            login: login
         };
-    });
+    }]);
