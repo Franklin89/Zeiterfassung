fd747e298c03829205fbbbc63c08a3e97d066df7
kreuzerk <kevin.kreuzer@hotmail.com>
2015-09-10 08:11:32 +0200

    kk: Inserting more alerts if an error occures

--- html5client/src/app/project/projectController.js
+++ html5client/src/app/project/projectController.js
@@ -2,45 +2,68 @@
  * Created by Kiwi on 07.09.15.
  */
 
-(function(){
+(function () {
 
     var timerecordingapp = angular.module('zeiterfassung.ui');
 
-    timerecordingapp.controller('ProjectController',['$scope', 'ProjectIntegrationService', '$timeout',
+    timerecordingapp.controller('ProjectController', ['$scope', 'ProjectIntegrationService', '$timeout',
         function ($scope, projectIntegrtionService, $timeout) {
 
-        $scope.projects;
-        $scope.projectNameInvalid = false;
-        $scope.projectInserted = false;
+            $scope.projects;
+            $scope.projectNameInvalid = false;
+            $scope.projectInserted = false;
+            $scope.tasknameInvalid = false;
+            $scope.taskInserted = false;
+            $scope.networkErrorOccured = false;
 
-        var readProjects = function() {
-            projectIntegrtionService.readPojects().then(function (result) {
-                $scope.projects = result;
-            });
-        };
+            var readProjects = function () {
+                projectIntegrtionService.readPojects().then(function (result) {
+                    $scope.projects = result;
+                });
+            };
 
-        $scope.addProject = function () {
+            $scope.addProject = function () {
 
-            if($scope.projectname == undefined){
-                $scope.projectNameInvalid = true;
-            }
-            else {
+                if ($scope.projectname == undefined) {
+                    $scope.projectNameInvalid = true;
+                }
+                else {
 
-                $scope.projectInserted = true;
+                    $scope.projectInserted = true;
 
-                $scope.projectNameInvalid = false;
-                projectIntegrtionService.createProject({
-                        "Name": $scope.projectname
-                    }
-                ).then(function(){
-                        $scope.projectInserted = true;
-                        $timeout(function(){
-                            $scope.projectInserted = false;
-                        }, 2000);
-                    });
-                $timeout(readProjects, 1000);
-            }
-        };
-        readProjects();
-    }]);
+                    $scope.projectNameInvalid = false;
+                    projectIntegrtionService.createProject({
+                            "Name": $scope.projectname
+                        }
+                    ).success(function () {
+                            $scope.projectInserted = true;
+                            $timeout(function () {
+                                $scope.projectInserted = false;
+                            }, 2000);
+                        }).error(function(){
+                            alert("Error");
+                            $scope.networkErrorOccured = true;
+                        });
+                    $timeout(readProjects, 1000);
+                }
+            };
+
+
+            $scope.addTask = function () {
+                if ($scope.taskname == undefined) {
+                    $scope.tasknameInvalid = true;
+                }
+                else{
+                    $scope.taskInserted = true;
+
+                    $scope.tasknameInvalid = false;
+
+
+
+
+                }
+            };
+
+            readProjects();
+        }]);
 })();
