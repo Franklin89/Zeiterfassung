805175e76edffc07521062e3c1e00c50a4403bad
Matteo Locher <matteo.locher@ml-software.ch>
2015-09-15 10:45:48 +0200

    Don't tell anyone muahah

---
+++ Db/CreateUsers.sql
@@ -0,0 +1,82 @@
+USE [School]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName]
+           ,[Email]
+           ,[Password]
+           ,[WorkingHoursPerDay]
+           ,[Username])
+     VALUES
+           ('Daniel',
+           'Jovanovic',
+           'daniel.jovanovic@students.bfh.ch',
+           'ecc4208a7778c1d76e7e89c5253128c5',
+           8.4,
+           'djovanovic')
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName]
+           ,[Email]
+           ,[Password]
+           ,[WorkingHoursPerDay]
+           ,[Username])
+     VALUES
+           ('Matteo',
+           'Locher',
+           'matteo.locher@students.bfh.ch',
+           'ecc4208a7778c1d76e7e89c5253128c5',
+           5.88,
+           'mlocher')
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName]
+           ,[Email]
+           ,[Password]
+           ,[WorkingHoursPerDay]
+           ,[Username])
+     VALUES
+           ('Nikalus',
+           'Schaerer',
+           'niklaus.schaerer@students.bfh.ch',
+           'ecc4208a7778c1d76e7e89c5253128c5',
+           6.72,
+           'nschaerer')
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName]
+           ,[Email]
+           ,[Password]
+           ,[WorkingHoursPerDay]
+           ,[Username])
+     VALUES
+           ('Kevin',
+           'Kreuzer',
+           'kevin.kreuzer@students.bfh.ch',
+           'ecc4208a7778c1d76e7e89c5253128c5',
+           8.4,
+           'kkreuzer')
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName]
+           ,[Email]
+           ,[Password]
+           ,[WorkingHoursPerDay]
+           ,[Username])
+     VALUES
+           ('Admin',
+           'Admin',
+           'admin.admin@students.bfh.ch',
+           '552b2ebe774bb5aaa0ad2021da259d22',
+           8.4,
+           'admin')
+GO
