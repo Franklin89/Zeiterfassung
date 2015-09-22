4dcf48547e9b4d0dde3f51f86d099f0899f8d13a
U110333 <niklaus.schaerer@mobi.ch>
2015-09-22 08:47:05 +0200

    Fix grunt

--- html5client/Gruntfile.js
+++ html5client/Gruntfile.js
@@ -62,7 +62,7 @@
             },
             watch: {
                 files: ['<%= jshint.files %>'],
-                tasks: ['jsh', 'cs']
+                tasks: ['test-jsh', 'test-cs']
             }
             //watch: {
             //    options: {
