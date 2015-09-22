3e358a32ffda77d79b5ff1f6e4b29c4f41f999d7
Daniel Jovanovic <daniel.jovanovic@geowerkstatt.ch>
2015-09-22 10:39:31 +0200

    add angular-loading-bar
    fixes #3

--- html5client/src/app/authentication/authenticationController.js
+++ html5client/src/app/authentication/authenticationController.js
@@ -1,17 +1,18 @@
 (function() {
     'use strict';
 
-    angular.module('zeiterfassung.authentication', ['zeiterfassung.authentication.integrationservices'])
+    angular.module('zeiterfassung.authentication',
+        ['zeiterfassung.authentication.integrationservices', 'angular-loading-bar'])
         .controller('AuthenticationController', ['$scope', '$state', 'AuthenticationIntegrationService',
             function($scope, $state, authenticationIntegrationService) {
                 $scope.login = function() {
                     authenticationIntegrationService.login($scope.user)
                         .then(function(result) {
                             // authorized
-                            if (authenticationIntegrationService.isAdmin()){
+                            if (authenticationIntegrationService.isAdmin()) {
                                 $state.go('projectManagement');
                             }
-                            else{
+                            else {
                                 $state.go('timeRecording');
                             }
                             return result;
