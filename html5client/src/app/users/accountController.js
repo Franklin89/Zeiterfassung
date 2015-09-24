c6f715ae934865bbffe5802c10be76ce5523f0c2
Daniel Jovanovic <daniel.jovanovic@geowerkstatt.ch>
2015-09-24 20:19:16 +0200

    add delete function
    #5

--- html5client/src/app/users/accountController.js
+++ html5client/src/app/users/accountController.js
@@ -42,6 +42,20 @@
                         }, function() {
                             // TODO:show error message
                         });
+
+                $scope.deleteUser = function(user) {
+                    swal({
+                        title: 'Benutzer l\u00f6schen',
+                        text: 'Soll der Benutzer ' + user.LastName + ' ' + user.FirstName + ' wirklich gel\u00f6scht werden?',
+                        type: 'warning',
+                        showCancelButton: true,
+                        confirmButtonText: 'Ja, l\u00f6schen',
+                        cancelButtonText: 'Nein',
+                        closeOnConfirm: true
+                    }, function() {
+                        usersIntegrationService.deleteUser(user.Id);
+                    });
+                };
             }
         ]);
 })();
