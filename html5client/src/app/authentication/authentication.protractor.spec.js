e97ecb122006d7ee7615a1103d8029ee8436c44e
U110333 <niklaus.schaerer@mobi.ch>
2015-09-28 10:44:24 +0200

    Added Logout and failed Login Test

--- html5client/src/app/authentication/authentication.protractor.spec.js
+++ html5client/src/app/authentication/authentication.protractor.spec.js
@@ -3,26 +3,49 @@
 
     describe('Zeiterfassung Login- Test', function() {
 
-        var pageToTestUrl, targetUrl, testUser, testUserPwd;
+        var pageToTestUrl, targetUrl, testUser, testUserPwd, failedPwd;
 
         beforeAll(function() {
             pageToTestUrl = 'http://localhost:63342/Zeiterfassung/html5client/src/app/index.html#/login';
-            targetUrl = 'http://localhost:63342/Zeiterfassung/html5client/src/app/index.html#/timeRecording';
+            targetUrl = 'http://localhost:63342/Zeiterfassung/html5client/src/app/index.html#/statistics';
             testUser = 'Admin';
             testUserPwd = 'Admin1234!';
+            failedPwd = 'smokeAfterSex'
         });
 
         beforeEach(function() {
             browser.get(pageToTestUrl);
+            element(by.id('username')).sendKeys(testUser);
+            element(by.id('password')).sendKeys(testUserPwd);
         });
 
         it('should test the login', function() {
-            var hittedUrl;
-            element(by.id('username')).sendKeys(testUser);
-            element(by.id('password')).sendKeys(testUserPwd);
+            var hittedUrlPromise;
             element(by.id('submitbutton')).click();
-            hittedUrl = browser.getCurrentUrl();
-            expect(hittedUrl).toEqual(targetUrl);
+            browser.waitForAngular();
+            hittedUrlPromise = browser.getCurrentUrl();
+            expect(hittedUrlPromise).toEqual(targetUrl);
+        });
+
+        it('should logout', function() {
+            var alerter, confirmButton, target;
+            element(by.id('submitbutton')).click();
+            browser.waitForAngular();
+            element(by.id('menuLogout')).click();
+            browser.waitForAngular();
+            alerter = element(by.css('.sweet-alert'));
+            alerter.element(by.css('.confirm')).click().then(function(){
+                browser.get(pageToTestUrl);
+                browser.waitForAngular();
+                expect(browser.getCurrentUrl()).toEqual(pageToTestUrl);
+            }, 3000);
+        });
+
+        it('should do a failed login', function(){
+            element(by.id('password')).sendKeys(failedPwd);
+            element(by.id('submitbutton')).click();
+            browser.waitForAngular();
+            expect(element(by.id('failedLogin')).isPresent()).toBe(true);
         });
     });
 })();
