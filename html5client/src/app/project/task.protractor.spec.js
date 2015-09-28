7bdcaaeae0048aaec24296d76800a4ce23e77368
U110333 <niklaus.schaerer@mobi.ch>
2015-09-28 14:59:47 +0200

    Added nice test to add and remove tasks

---
+++ html5client/src/app/project/task.protractor.spec.js
@@ -0,0 +1,70 @@
+(function() {
+    'use strict';
+
+    describe('Project Erfassung - Test', function() {
+
+        var pageToTestUrl, testUser, taskToAddName, testUserPwd, projectToAddName, projectName, deleteButton;
+
+        beforeAll(function() {
+            var dropDownList, project;
+            pageToTestUrl = 'http://localhost:63342/Zeiterfassung/html5client/src/app/index.html#/login';
+            testUser = 'Admin';
+            testUserPwd = 'Admin1234!';
+            projectToAddName = 'ShivaProject';
+            taskToAddName = 'ShivasTask';
+            browser.get(pageToTestUrl);
+            element(by.id('username')).sendKeys(testUser);
+            element(by.id('password')).sendKeys(testUserPwd);
+            element(by.id('submitbutton')).click();
+            browser.waitForAngular();
+            element(by.id('menuProjectManagement')).click();
+            browser.waitForAngular();
+            element(by.id('projectNameInput')).sendKeys(projectToAddName);
+            element(by.id('addButton')).click();
+            browser.waitForAngular();
+            //Choose project to Add Task
+            element(by.id('dropdownMenu1')).click();
+            browser.waitForAngular();
+            element(by.id('dropdownMenuMenu')).all(by.repeater('project in projects')).last().click();
+            browser.waitForAngular();
+            element(by.id('tasknameInput')).sendKeys(taskToAddName);
+            element(by.id('tasknameInputSubmit')).click();
+            browser.waitForAngular();
+        });
+
+        afterAll(function(){
+            var newProject, deleteButton;
+            newProject = element.all(by.repeater('project in projects')).last();
+            deleteButton = newProject.element(by.css('td > div > a'));
+            deleteButton.click();
+            browser.waitForAngular();
+        });
+
+        it('should find the created Task', function(){
+            var newProject, newTask, inputText;
+            newProject = element.all(by.repeater('project in projects')).last();
+            newTask = newProject.all(by.repeater('task in project.Tasks')).last();
+            inputText = newTask.element(by.model('task.Name'));
+            expect(inputText.getAttribute('value')).toEqual(taskToAddName);
+        });
+
+        it('should not display the project delete button', function(){
+            var newProject, newTask, inputText;
+            newProject = element.all(by.repeater('project in projects')).last();
+            newProject.element(by.css('td > div > a')).isDisplayed().then(function(displayed){
+                expect(displayed).toBe(false);
+            });
+        });
+
+        it('should delete the the task', function(){
+            var newProject, newTask, deleteButton;
+            newProject = element.all(by.repeater('project in projects')).last();
+            newTask = newProject.all(by.repeater('task in project.Tasks')).last();
+            deleteButton = newTask.element(by.css('div a'));
+            deleteButton.click();
+            browser.waitForAngular();
+            newProject = element.all(by.repeater('project in projects')).last();
+            expect(newProject.all(by.repeater('task in project.Tasks')).count()).toBe(0);
+        });
+    });
+})();
